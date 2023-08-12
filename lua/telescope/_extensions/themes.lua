9332feb0b1ebc2b18bb9698ce519ec49d55f8d15
Andrew Berty <95014217+andrew-george@users.noreply.github.com>
2023-08-12 20:08:52 +0300

    Create themes.lua

---
+++ lua/telescope/_extensions/themes.lua
@@ -0,0 +1,64 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local previewers = require "telescope.previewers"
+local sorters = require "telescope.sorters"
+
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+
+local function switcher()
+  local mini = {
+    layout_strategy = 'vertical',
+    layout_config = {
+      height = 0.5,
+      width = 0.3,
+    }
+  }
+
+  local function set_theme()
+    local selected = action_state.get_selected_entry()[1]
+    local cmd = 'colorscheme ' .. selected
+    vim.cmd(cmd)
+  end
+
+  local colors = vim.fn.getcompletion('', 'color')
+
+
+  local picker_opts = {
+    prompt_title = "Theme Switcher",
+    -- previewer = previewer,
+    finder = finders.new_table(colors),
+    sorter = sorters.get_generic_fuzzy_sorter({}),
+    sorting_strategy = 'ascending',
+
+
+    attach_mappings = function(prompt_bufnr, map)
+      map('i', '<CR>', function()
+        set_theme()
+        actions.close(prompt_bufnr)
+      end)
+
+      -- reload theme on cycling
+      map("i", "<Down>", function()
+        actions.move_selection_next(prompt_bufnr)
+        set_theme()
+      end)
+
+      map("i", "<Up>", function()
+        actions.move_selection_previous(prompt_bufnr)
+        set_theme()
+      end)
+
+      return true
+    end,
+  }
+
+  local picker = pickers.new(mini, picker_opts)
+  picker:find()
+end
+
+return require("telescope").register_extension {
+  exports = { themes = switcher },
+}
