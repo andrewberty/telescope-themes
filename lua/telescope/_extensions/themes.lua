cc6ae4304ab442843830561b774aa16758ca2b1e
Andrew Berty <andrewgeorgeberty@gmail.com>
2024-01-28 18:25:52 +0200

    integrate previewer pane and respect telescope default options

--- lua/telescope/_extensions/themes.lua
+++ lua/telescope/_extensions/themes.lua
@@ -1,73 +1,71 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local previewers = require "telescope.previewers"
-local sorters = require "telescope.sorters"
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local previewers = require("telescope.previewers")
+local sorters = require("telescope.sorters")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
-local conf = require("telescope.config").values
-local actions = require "telescope.actions"
-local action_state = require "telescope.actions.state"
+local function switcher(opts)
+	local function set_theme()
+		vim.cmd("colorscheme " .. action_state.get_selected_entry()[1])
+	end
 
+	local function write_config()
+		local selected = action_state.get_selected_entry()[1]
+		local theme_path = vim.fn.stdpath("config") .. "/lua/current-theme.lua"
+		file = io.open(theme_path, "w")
+		file:write('vim.cmd("colorscheme ' .. selected .. '")')
+		file:close()
+	end
 
-local function switcher()
-  local mini = {
-    layout_strategy = 'vertical',
-    layout_config = {
-      height = 0.5,
-      width = 0.3,
-    }
-  }
+	local colors = vim.fn.getcompletion("", "color")
+	local bufnr = vim.api.nvim_get_current_buf()
 
-  local function set_theme()
-    local selected = action_state.get_selected_entry()[1]
-    local cmd = 'colorscheme ' .. selected
-    vim.cmd(cmd)
-  end
+	local previewer = previewers.new_buffer_previewer({
+		define_preview = function(self, entry)
+			-- add content
+			local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+			vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
 
-  local function write_config()
-    local selected = action_state.get_selected_entry()[1]
-    local theme_path = vim.fn.stdpath "config" .. "/lua/current-theme.lua"
-    file = io.open(theme_path, "w")
-    file:write('vim.cmd("colorscheme ' .. selected .. '")')
-    file:close()
-  end
+			-- add syntax highlighting in previewer
+			local ft = (vim.filetype.match({ buf = bufnr }) or "diff"):match("%w+")
+			require("telescope.previewers.utils").highlighter(self.state.bufnr, ft)
+		end,
+	})
 
-  local colors = vim.fn.getcompletion('', 'color')
+	local picker_opts = {
+		prompt_title = "Themes",
+		previewer = previewer,
+		finder = finders.new_table(colors),
+		sorter = sorters.get_generic_fuzzy_sorter(opts),
+		sorting_strategy = "ascending",
 
+		attach_mappings = function(prompt_bufnr, map)
+			map("i", "<CR>", function()
+				set_theme()
+				write_config()
+				actions.close(prompt_bufnr)
+			end)
 
-  local picker_opts = {
-    prompt_title = "Themes",
-    -- previewer = previewer,
-    finder = finders.new_table(colors),
-    sorter = sorters.get_generic_fuzzy_sorter({}),
-    sorting_strategy = 'ascending',
+			-- reload theme on cycling
+			map("i", "<Down>", function()
+				actions.move_selection_next(prompt_bufnr)
+				set_theme()
+			end)
 
+			map("i", "<Up>", function()
+				actions.move_selection_previous(prompt_bufnr)
+				set_theme()
+			end)
 
-    attach_mappings = function(prompt_bufnr, map)
-      map('i', '<CR>', function()
-        set_theme()
-        write_config()
-        actions.close(prompt_bufnr)
-      end)
+			return true
+		end,
+	}
 
-      -- reload theme on cycling
-      map("i", "<Down>", function()
-        actions.move_selection_next(prompt_bufnr)
-        set_theme()
-      end)
-
-      map("i", "<Up>", function()
-        actions.move_selection_previous(prompt_bufnr)
-        set_theme()
-      end)
-
-      return true
-    end,
-  }
-
-  local picker = pickers.new(mini, picker_opts)
-  picker:find()
+	local picker = pickers.new(opts, picker_opts)
+	picker:find()
 end
 
-return require("telescope").register_extension {
-  exports = { themes = switcher },
-}
+return require("telescope").register_extension({
+	exports = { themes = switcher },
+})
